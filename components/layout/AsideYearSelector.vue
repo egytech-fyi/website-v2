3b0b4cbd075f6c73fc0eb3930fb3b92088f94aa5
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 00:02:17 +0300

    feat: `AsideYearSelector` component

---
+++ components/layout/AsideYearSelector.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  // Getting the available years to add them as options by querying the `/content` folder
+  const { getAvailableContentYears } = useContentDate()
+  const availableContentYears = await getAvailableContentYears()
+
+  // Assigning the default value from current path year and fallback to latest available content year
+  const { currentPathYear } = useContentDate()
+  const selectedYear = ref(currentPathYear.value || availableContentYears[0])
+
+  // Updating the path on selecting a different year to show the corresponding content
+  const { currentPathWithoutYear } = useContentDate()
+  function navigateToSelectedYearContent() {
+    const newPath = `/${selectedYear.value}/${currentPathWithoutYear.value}`
+    return navigateTo(newPath)
+  }
+</script>
+
+<template>
+  <UISelect
+    id="content-year-selector"
+    v-model="selectedYear"
+    @update:model-value="navigateToSelectedYearContent"
+  >
+    <UISelectTrigger class="[&>span]:overflow-visible">
+      <UISelectValue placeholder="Select a year" :value="2" />
+    </UISelectTrigger>
+
+    <UISelectContent>
+      <UISelectItem
+        v-for="year in availableContentYears"
+        :key="year"
+        :value="year"
+      >
+        {{ year }}
+        <span
+          v-if="year === availableContentYears[0]"
+          class="ms-1 rounded border bg-background p-px px-1 py-0.5 text-xs text-foreground"
+        >
+          Latest
+        </span>
+      </UISelectItem>
+    </UISelectContent>
+  </UISelect>
+</template>
