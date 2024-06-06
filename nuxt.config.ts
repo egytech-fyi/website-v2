c11fffebe779f5e0af58c3e62b546a8259169fb7
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-06 23:30:07 +0300

    chore: add 'shadcn-nuxt' and 'shadcn-vue'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
 
   // Generate only Nuxt-specific rules as we're using `antfu/eslint-config` for other Vue & TS rules
   eslint: { config: { standalone: false } },
+
+  shadcn: { prefix: 'UI', componentDir: './components/ui' },
 })
