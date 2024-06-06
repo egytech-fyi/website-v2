68172bbfed2da0e7cf7afa336abb6395e18364ce
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-06 19:25:09 +0300

    fix: disable `nuxt/eslint` standalone config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   modules: ['@nuxt/eslint'],
+
+  // Generate only Nuxt-specific rules as we're using `antfu/eslint-config` for other Vue & TS rules
+  eslint: { config: { standalone: false } },
 })
