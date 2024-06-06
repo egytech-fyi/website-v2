4f37c313863d936f77d5438c6c52313ea150de54
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-06 20:16:40 +0300

    chore: format non-md files

--- app.config.ts
+++ app.config.ts
@@ -14,7 +14,7 @@ const defaultCodeIcon = {
   '.env': 'vscode-icons:file-type-dotenv',
   '.env.example': 'vscode-icons:file-type-dotenv',
   '.vscode/settings.json': 'vscode-icons:file-type-vscode',
-  'nuxt': 'vscode-icons:file-type-nuxt',
+  nuxt: 'vscode-icons:file-type-nuxt',
   '.nuxtrc': 'vscode-icons:file-type-nuxt',
   '.nuxtignore': 'vscode-icons:file-type-nuxt',
   'nuxt.config.js': 'vscode-icons:file-type-nuxt',
@@ -22,30 +22,30 @@ const defaultCodeIcon = {
   'nuxt.schema.ts': 'vscode-icons:file-type-nuxt',
   'tailwind.config.js': 'vscode-icons:file-type-tailwind',
   'tailwind.config.ts': 'vscode-icons:file-type-tailwind',
-  'vue': 'vscode-icons:file-type-vue',
-  'ts': 'vscode-icons:file-type-typescript',
-  'tsx': 'vscode-icons:file-type-typescript',
-  'mjs': 'vscode-icons:file-type-js',
-  'cjs': 'vscode-icons:file-type-js',
-  'js': 'vscode-icons:file-type-js',
-  'jsx': 'vscode-icons:file-type-js',
-  'md': 'vscode-icons:file-type-markdown',
-  'mdc': 'vscode-icons:file-type-markdown',
-  'py': 'vscode-icons:file-type-python',
-  'npm': 'vscode-icons:file-type-npm',
-  'pnpm': 'vscode-icons:file-type-pnpm',
-  'npx': 'vscode-icons:file-type-npm',
-  'yarn': 'vscode-icons:file-type-yarn',
-  'bun': 'vscode-icons:file-type-bun',
-  'yml': 'vscode-icons:file-type-yaml',
-  'json': 'vscode-icons:file-type-json',
-  'terminal': 'lucide:terminal',
-};
+  vue: 'vscode-icons:file-type-vue',
+  ts: 'vscode-icons:file-type-typescript',
+  tsx: 'vscode-icons:file-type-typescript',
+  mjs: 'vscode-icons:file-type-js',
+  cjs: 'vscode-icons:file-type-js',
+  js: 'vscode-icons:file-type-js',
+  jsx: 'vscode-icons:file-type-js',
+  md: 'vscode-icons:file-type-markdown',
+  mdc: 'vscode-icons:file-type-markdown',
+  py: 'vscode-icons:file-type-python',
+  npm: 'vscode-icons:file-type-npm',
+  pnpm: 'vscode-icons:file-type-pnpm',
+  npx: 'vscode-icons:file-type-npm',
+  yarn: 'vscode-icons:file-type-yarn',
+  bun: 'vscode-icons:file-type-bun',
+  yml: 'vscode-icons:file-type-yaml',
+  json: 'vscode-icons:file-type-json',
+  terminal: 'lucide:terminal',
+}
 
 export default defineAppConfig({
   shadcnDocs: {
     site: {
-      name: 'shadcn-docs-nuxt'
+      name: 'shadcn-docs-nuxt',
     },
     header: {
       title: 'shadcn-docs-starter',
@@ -56,11 +56,13 @@ export default defineAppConfig({
         dark: '/logo-dark.svg',
       },
       nav: [],
-      links: [{
-        icon: 'lucide:github',
-        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
-        target: '_blank',
-      }],
+      links: [
+        {
+          icon: 'lucide:github',
+          to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+          target: '_blank',
+        },
+      ],
     },
     aside: {
       useLevel: true,
@@ -73,30 +75,35 @@ export default defineAppConfig({
     },
     footer: {
       credits: 'Copyright © 2024',
-      links: [{
-        icon: 'lucide:github',
-        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
-        target: '_blank',
-      }],
+      links: [
+        {
+          icon: 'lucide:github',
+          to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+          target: '_blank',
+        },
+      ],
     },
     toc: {
       enable: true,
       title: 'On This Page',
-      links: [{
-        title: 'Star on GitHub',
-        icon: 'lucide:star',
-        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
-        target: '_blank',
-      }, {
-        title: 'Create Issues',
-        icon: 'lucide:circle-dot',
-        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt/issues',
-        target: '_blank',
-      }],
+      links: [
+        {
+          title: 'Star on GitHub',
+          icon: 'lucide:star',
+          to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+          target: '_blank',
+        },
+        {
+          title: 'Create Issues',
+          icon: 'lucide:circle-dot',
+          to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt/issues',
+          target: '_blank',
+        },
+      ],
     },
     search: {
       enable: true,
       inAside: false,
-    }
-  }
-});
+    },
+  },
+})
