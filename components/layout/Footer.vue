8c22db4b3df7b6540c11b8972f006eae628d5be1
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-06 20:55:16 +0300

    feat: override default Footer

---
+++ components/layout/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const currentYear = new Date().getFullYear().toString()
+</script>
+
+<template>
+  <footer class="border-t py-3">
+    <div
+      class="container flex max-w-screen-2xl flex-col items-center justify-between gap-3 xs:flex-row"
+    >
+      <LayoutHeaderLogo class="hidden flex-1 md:flex" />
+      <p>© {{ currentYear }} All Rights Reserved</p>
+    </div>
+  </footer>
+</template>
