607569ab5c0905dbaeeb6ba415852b8a9d563251
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 14:46:08 +0300

    feat: Link component

---
+++ components/ui/link/Link.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import type { HTMLAttributes } from 'vue'
+  import { type ButtonVariants, buttonVariants } from '.'
+  import type { NuxtLinkProps } from '#app'
+  import { cn } from '~/lib/utils'
+
+  type Props = {
+    variant?: ButtonVariants['variant']
+    size?: ButtonVariants['size']
+    class?: HTMLAttributes['class']
+    prefetch?: boolean
+  } & Omit<NuxtLinkProps, 'prefetch' | 'noPrefetch'>
+
+  const props = defineProps<Props>()
+</script>
+
+<template>
+  <NuxtLink
+    :class="
+      cn(buttonVariants({ variant, size }), 'hover:cursor-pointer', props.class)
+    "
+    v-bind="$props"
+  >
+    <slot />
+  </NuxtLink>
+</template>
