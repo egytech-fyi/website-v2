b41028750186aa13e4467cc372605ad8b03ef68d
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 17:55:35 +0300

    fix: parse year out of current path

--- composables/useContentDateUtils.ts
+++ composables/useContentDateUtils.ts
@@ -39,7 +39,7 @@ export default function () {
   })
 
   const isInsideYearDir = computed(() => {
-    return isPathContainsYear(route.path)
+    return isPathContainsYear(currentPathYear.value)
   })
 
   return {
