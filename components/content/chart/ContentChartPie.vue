6eae2a10bf7c0a97f2849efb05a2a746e7b76b75
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 14:18:27 +0300

    feat: ContentChartPie component

---
+++ components/content/chart/ContentChartPie.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import type { ApexOptions } from 'apexcharts'
+  const {
+    series = [],
+    labels = [],
+    options = {},
+  } = defineProps<{
+    series: ApexAxisChartSeries
+    labels: string[]
+    options?: ApexOptions
+  }>()
+</script>
+
+<template>
+  <ContentChart
+    :series
+    :options="{
+      chart: { type: 'pie' },
+      labels,
+      ...options,
+    }"
+  />
+</template>
