2e3c8711375df83e32ce82acbe751093444a679e
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 18:44:02 +0300

    chore: setup 'nuxt/seo'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,51 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
-  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@nuxt/fonts', '@nuxtjs/seo'],
+
+  // SEO
+  site: {
+    url: 'https://egytech.fyi/',
+    name: 'egytech.fyi',
+    description: `Explore the latest trends in the Egyptian tech market, analyze salaries, and get personalized negotiation strategies based on your CV.`,
+    defaultLocale: 'en',
+  },
+  app: {
+    head: {
+      // Favicon
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '192x192',
+          href: '/android-chrome-192x192.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '512x512',
+          href: '/android-chrome-512x512.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+      ],
+    },
+  },
 
   // Generate only Nuxt-specific rules as we're using `antfu/eslint-config` for other Vue & TS rules
   eslint: { config: { standalone: false } },
