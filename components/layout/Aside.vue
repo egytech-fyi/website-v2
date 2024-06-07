ebc880b5e26699c2241dcb3a1461ef654394b10f
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 21:01:13 +0300

    feat: support both dirs & files in aside

--- components/layout/Aside.vue
+++ components/layout/Aside.vue
@@ -8,7 +8,7 @@
   const { currentPathYear, isInsideYearDir, getAvailableContentYears } =
     useContentDateUtils()
   const availableContentYears = await getAvailableContentYears()
-  const yearTree = computed(() => {
+  const yearDirTree = computed(() => {
     // Show current year tree if inside year dir, otherwise show the latest available year tree
     const activeYear = isInsideYearDir.value
       ? currentPathYear.value
@@ -18,8 +18,15 @@
   })
 
   // Other files (not in year format or index page) tree
-  const nonYearTree = computed(() => {
-    return navigation.value.filter((dir: any) => !isPathContainsYear(dir._path))
+  const nonYearDirsTree = computed(() => {
+    return navigation.value.filter(
+      (dir: any) => !isPathContainsYear(dir._path) && dir.children?.length,
+    )
+  })
+  const nonYearFilesTree = computed(() => {
+    return navigation.value.filter(
+      (dir: any) => !isPathContainsYear(dir._path) && !dir.children,
+    )
   })
 </script>
 
@@ -30,76 +37,123 @@
     type="hover"
   >
     <div class="space-y-4">
-      <LayoutAsideYearSelector />
+      <!-- Survey -->
+      <div class="space-y-3">
+        <p class="font-medium">Survey</p>
 
-      <!-- Year Dir Tree -->
-      <ul v-if="yearTree" class="space-y-3">
-        <li v-for="link in yearTree.children" :key="link._path">
-          <NuxtLink
-            :to="link._path"
-            class="flex items-center gap-2"
-            :class="[
-              !path.startsWith(link._path) &&
-                'text-muted-foreground transition-colors hover:text-primary/80',
-            ]"
-          >
-            <div
-              size="16"
-              class="flex rounded border p-1"
-              :class="{
-                'bg-primary text-primary-foreground': path.startsWith(
-                  link._path,
-                ),
-              }"
+        <LayoutAsideYearSelector />
+
+        <!-- Year Dir Tree -->
+        <ul v-if="yearDirTree" class="space-y-3">
+          <li v-for="link in yearDirTree.children" :key="link._path">
+            <NuxtLink
+              :to="link._path"
+              class="flex items-center gap-2"
+              :class="[
+                !path.startsWith(link._path) &&
+                  'text-muted-foreground transition-colors hover:text-primary/80',
+              ]"
             >
-              <Icon
-                :name="link.icon || 'ph:folder-notch-open-duotone'"
-                class="size-5"
-              />
-            </div>
+              <div
+                size="16"
+                class="flex rounded border p-1"
+                :class="{
+                  'bg-primary text-primary-foreground': path.startsWith(
+                    link._path,
+                  ),
+                }"
+              >
+                <Icon
+                  :name="link.icon || 'ph:folder-notch-open-duotone'"
+                  class="size-5"
+                />
+              </div>
 
-            <span class="font-medium">
-              {{ link.title }}
-            </span>
-          </NuxtLink>
-        </li>
-      </ul>
+              <span class="font-medium">
+                {{ link.title }}
+              </span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
 
-      <!-- Non Year Tree -->
-      <ul
-        v-if="nonYearTree.length"
-        class="space-y-3 border-t border-dashed pt-3"
-      >
-        <li v-for="link in nonYearTree" :key="link._path">
-          <NuxtLink
-            :to="link._path"
-            class="flex items-center gap-2"
-            :class="[
-              !path.startsWith(link._path) &&
-                'text-muted-foreground transition-colors hover:text-primary/80',
-            ]"
-          >
-            <div
-              size="16"
-              class="flex rounded border p-1"
-              :class="{
-                'bg-primary text-primary-foreground': path.startsWith(
-                  link._path,
-                ),
-              }"
+      <!-- Non Year Dir Tree -->
+      <template v-if="nonYearDirsTree.length">
+        <ul
+          v-for="dir in nonYearDirsTree"
+          :key="dir._path"
+          class="space-y-3 border-t border-dashed pt-3"
+        >
+          <p class="font-medium">{{ dir.title }}</p>
+
+          <li v-for="link in dir.children" :key="link._path">
+            <NuxtLink
+              :to="link._path"
+              class="flex items-center gap-2"
+              :class="[
+                !path.startsWith(link._path) &&
+                  'text-muted-foreground transition-colors hover:text-primary/80',
+              ]"
             >
-              <Icon
-                :name="link.icon || 'ph:folder-notch-open-duotone'"
-                class="size-5"
-              />
-            </div>
+              <div
+                size="16"
+                class="flex rounded border p-1"
+                :class="{
+                  'bg-primary text-primary-foreground': path.startsWith(
+                    link._path,
+                  ),
+                }"
+              >
+                <Icon
+                  :name="link.icon || 'ph:folder-notch-open-duotone'"
+                  class="size-5"
+                />
+              </div>
 
-            <span class="font-medium">
-              {{ link.title }}
-            </span>
-          </NuxtLink>
-        </li>
-      </ul>
+              <span class="font-medium">
+                {{ link.title }}
+              </span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </template>
+
+      <!-- Non Year Files Tree -->
+      <template v-if="nonYearFilesTree.length">
+        <ul class="space-y-3">
+          <p class="font-medium">Other</p>
+
+          <li v-for="link in nonYearFilesTree" :key="link._path">
+            <NuxtLink
+              :to="link._path"
+              class="flex items-center gap-2"
+              :class="[
+                !path.startsWith(link._path) &&
+                  'text-muted-foreground transition-colors hover:text-primary/80',
+              ]"
+            >
+              <div
+                size="16"
+                class="flex rounded border p-1"
+                :class="{
+                  'bg-primary text-primary-foreground': path.startsWith(
+                    link._path,
+                  ),
+                }"
+              >
+                <Icon
+                  :name="link.icon || 'ph:folder-notch-open-duotone'"
+                  class="size-5"
+                />
+              </div>
+
+              <span class="font-medium">
+                {{ link.title }}
+              </span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </template>
 
       <!-- Extra Links -->
       <ul
