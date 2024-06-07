35573e5e0838a2197739bae81ee1926a90c236a2
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 14:18:21 +0300

    feat: ContentChartBarLine component

---
+++ components/content/chart/ContentChartBarLine.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  import type { ApexOptions } from 'apexcharts'
+  const {
+    labels,
+    barSeries,
+    lineSeries,
+    barTitle,
+    lineTitle,
+    barAxisLabelPrefix,
+    lineAxisLabelPrefix,
+    options = {},
+  } = defineProps<{
+    labels: string[]
+    barSeries: ApexAxisChartSeries
+    lineSeries: ApexAxisChartSeries[number]
+    barTitle?: string
+    lineTitle?: string
+    barAxisLabelPrefix?: string
+    lineAxisLabelPrefix?: string
+    options?: ApexOptions
+  }>()
+
+  const mappedBarSeries = computed(() => {
+    return barSeries.map((series) => ({ type: 'bar', ...series }))
+  })
+
+  const mergedOptions = computed<ApexOptions>(() => ({
+    xaxis: { categories: labels },
+    yaxis: [
+      {
+        title: { text: barTitle ?? barSeries[0].name, offsetX: -8 },
+        labels: {
+          formatter: (value: number) => `${+value}${barAxisLabelPrefix ?? ''}`,
+        },
+      },
+      {
+        title: { text: lineTitle ?? lineSeries.name, offsetX: 8 },
+        labels: {
+          formatter: (value: number) => `${+value}${lineAxisLabelPrefix ?? ''}`,
+        },
+        opposite: true,
+      },
+    ],
+    stroke: { width: [...Array(barSeries.length).fill(0), 4] }, // Remove stroke from bars and add to line only
+    dataLabels: {
+      enabled: true,
+      enabledOnSeries: [1],
+      formatter: (value: number) => `${+value}${lineAxisLabelPrefix ?? ''}`,
+    },
+    ...options,
+  }))
+</script>
+
+<template>
+  <ContentChart
+    :series="[...mappedBarSeries, { type: 'line', ...lineSeries }]"
+    :options="mergedOptions"
+  />
+</template>
