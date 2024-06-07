b76adef931cf1650c6c930488565b79dba9b86a0
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-07 18:28:09 +0300

    chore: improve components registration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,15 @@ export default defineNuxtConfig({
 
   // Generate only Nuxt-specific rules as we're using `antfu/eslint-config` for other Vue & TS rules
   eslint: { config: { standalone: false } },
+
+  // https://github.com/nuxt/nuxt/pull/22558
+  hooks: {
+    'components:extend': (components) => {
+      components.forEach((component) => {
+        if (component.global) {
+          component.global = 'sync'
+        }
+      })
+    },
+  },
 })
