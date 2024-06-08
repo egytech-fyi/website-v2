6a11919bb8a06d41ea5d816b67ca4e61cbd2cd15
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-08 19:12:41 +0300

    feat: add Number Field, Popover, Tags Input components from shadcn

---
+++ components/ui/number-field/NumberFieldDecrement.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import type { NumberFieldDecrementProps } from 'radix-vue'
+  import { NumberFieldDecrement, useForwardProps } from 'radix-vue'
+  import { type HTMLAttributes, computed } from 'vue'
+  import { Minus } from 'lucide-vue-next'
+  import { cn } from '@/lib/utils'
+
+  const props = defineProps<
+    NumberFieldDecrementProps & { class?: HTMLAttributes['class'] }
+  >()
+
+  const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props
+
+    return delegated
+  })
+
+  const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <NumberFieldDecrement
+    v-bind="forwarded"
+    :class="
+      cn(
+        'absolute top-1/2 -translate-y-1/2 left-0 p-3 disabled:cursor-not-allowed disabled:opacity-20',
+        props.class,
+      )
+    "
+  >
+    <slot>
+      <Minus class="size-4" />
+    </slot>
+  </NumberFieldDecrement>
+</template>
