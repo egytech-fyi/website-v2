3f65a886144d42200023323120a6e554d5d4f5da
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-09 20:24:36 +0300

    feat: TagsCombobox component

---
+++ components/ui/tags-combobox/TagsCombobox.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+  import {
+    ComboboxAnchor,
+    ComboboxInput,
+    ComboboxPortal,
+    ComboboxRoot,
+    ComboboxTrigger,
+  } from 'radix-vue'
+
+  type UiTagsComboboxProps = {
+    options: Array<{ value: string; label: string }>
+    placeholder?: string
+  }
+  const { options, placeholder = '' } = defineProps<UiTagsComboboxProps>()
+
+  // selectedOptions model value (consumed in parent by v-model directive)
+  const selectedOptions = defineModel<string[]>({ default: () => [] })
+
+  // Combobox state
+  const isComboboxOpen = ref(false)
+  const searchTerm = ref('')
+  const filteredOptions = computed(() =>
+    options.filter((i) => !selectedOptions.value.includes(i.label)),
+  )
+
+  // Updating selected options
+  type AcceptableValue = string | number | boolean | Record<string, any>
+  type SelectEvent<T> = CustomEvent<{
+    originalEvent: PointerEvent
+    value?: T
+  }>
+  function handleOptionSelect(ev: SelectEvent<AcceptableValue>) {
+    if (typeof ev.detail.value === 'string') {
+      searchTerm.value = ''
+      selectedOptions.value.push(ev.detail.value)
+    }
+
+    if (filteredOptions.value.length === 0) {
+      isComboboxOpen.value = false
+    }
+  }
+</script>
+
+<template>
+  <UiTagsInput class="w-full gap-0 px-0" :model-value="selectedOptions">
+    <div class="flex flex-wrap items-center gap-2 px-3">
+      <UiTagsInputItem
+        v-for="item in selectedOptions"
+        :key="item"
+        :value="item"
+      >
+        <UiTagsInputItemText />
+        <UiTagsInputItemDelete />
+      </UiTagsInputItem>
+    </div>
+
+    <ComboboxRoot
+      v-model="selectedOptions"
+      v-model:open="isComboboxOpen"
+      v-model:searchTerm="searchTerm"
+      class="w-full"
+    >
+      <ComboboxAnchor class="flex items-center px-2">
+        <ComboboxInput :placeholder as-child>
+          <UiTagsInputInput
+            class="w-full px-3"
+            :class="selectedOptions.length > 0 ? 'mt-2' : ''"
+          />
+
+          <ComboboxTrigger>
+            <Icon name="radix-icons:chevron-down" class="size-4" />
+          </ComboboxTrigger>
+        </ComboboxInput>
+      </ComboboxAnchor>
+
+      <ComboboxPortal>
+        <UiCommandList
+          position="popper"
+          class="mt-2 w-[--radix-popper-anchor-width] rounded-md border bg-popover text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+        >
+          <UiCommandEmpty />
+          <UiCommandGroup>
+            <UiCommandItem
+              v-for="option in filteredOptions"
+              :key="option.value"
+              :value="option.label"
+              @select.prevent="handleOptionSelect"
+            >
+              {{ option.label }}
+            </UiCommandItem>
+          </UiCommandGroup>
+        </UiCommandList>
+      </ComboboxPortal>
+    </ComboboxRoot>
+  </UiTagsInput>
+</template>
