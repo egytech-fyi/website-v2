5202f06f7978a0af318168bb725d9dd77ef71f94
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-09 14:31:53 +0300

    feat: ContentChartLine component

---
+++ components/content/chart/ContentChartLine.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import type { ApexOptions } from 'apexcharts'
+  const {
+    labels = [],
+    series = [],
+    options = {},
+  } = defineProps<{
+    labels: string[]
+    series: ApexAxisChartSeries
+    options?: ApexOptions
+  }>()
+
+  const mergedOptions = computed<ApexOptions>(() => ({
+    chart: { type: 'line' },
+    xaxis: { categories: labels },
+    ...options,
+  }))
+</script>
+
+<template>
+  <ContentChart :series :options="mergedOptions" />
+</template>
