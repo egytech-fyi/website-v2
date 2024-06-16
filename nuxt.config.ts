aeb854a152d1ef0c6f406d4d36447c35005459ed
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-06-16 14:22:19 +0300

    chore: move 'devtools' key to bottom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   modules: [
     '@nuxt/eslint',
@@ -76,4 +75,6 @@ export default defineNuxtConfig({
       })
     },
   },
+
+  devtools: { enabled: true },
 })
