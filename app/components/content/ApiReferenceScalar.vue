516d5332a67b726f46e4fd675f1e441166edca2e
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-07-10 22:53:19 +0300

    style: ensure all styles are applied in dark mode

--- app/components/content/ApiReferenceScalar.vue
+++ app/components/content/ApiReferenceScalar.vue
@@ -46,26 +46,29 @@
     }
 
     /* Matching Scalar documentation styles with ours */
-    --scalar-background-1: hsl(var(--background));
-    --scalar-background-2: hsl(var(--background));
-    --scalar-background-3: hsl(var(--background));
-    --scalar-border-color: hsl(var(--border));
-    --scalar-background-accent: hsl(var(--muted));
-    --scalar-color-accent: hsl(var(--muted-foreground));
+    &,
+    .dark-mode {
+      --scalar-background-1: hsl(var(--background));
+      --scalar-background-2: hsl(var(--background));
+      --scalar-background-3: hsl(var(--secondary));
+      --scalar-border-color: hsl(var(--border));
+      --scalar-background-accent: hsl(var(--muted));
+      --scalar-color-accent: hsl(var(--muted-foreground));
 
-    --scalar-color-1: hsl(var(--primary));
-    --scalar-color-2: hsl(var(--muted-foreground));
-    --scalar-color-3: hsl(var(--primary));
+      --scalar-color-1: hsl(var(--primary));
+      --scalar-color-2: hsl(var(--muted-foreground));
+      --scalar-color-3: hsl(var(--primary));
 
-    --scalar-button-1: hsl(var(--secondary));
-    --scalar-button-1-hover: hsl(var(--secondary) / 0.8);
-    --scalar-button-1-color: hsl(var(--secondary-foreground));
+      --scalar-button-1: hsl(var(--background));
+      --scalar-button-1-hover: hsl(var(--background) / 0.8);
+      --scalar-button-1-color: hsl(var(--foreground));
 
-    --scalar-color-green: #17803d;
-    --scalar-color-red: #e10909;
-    --scalar-color-yellow: #edbe20;
-    --scalar-color-blue: #1763a6;
-    --scalar-color-orange: #e25b09;
-    --scalar-color-purple: #5c3993;
+      --scalar-color-green: #17803d;
+      --scalar-color-red: #e10909;
+      --scalar-color-yellow: #edbe20;
+      --scalar-color-blue: #1763a6;
+      --scalar-color-orange: #e25b09;
+      --scalar-color-purple: #5c3993;
+    }
   }
 </style>
