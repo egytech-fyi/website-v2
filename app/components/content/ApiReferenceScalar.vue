dea8655406a937fb46de46f6bd20fffbac66ecd9
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-07-10 17:54:07 +0300

    chore: format

--- app/components/content/ApiReferenceScalar.vue
+++ app/components/content/ApiReferenceScalar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  import { ApiReference } from '@scalar/api-reference'
   import type { ReferenceConfiguration } from '@scalar/api-reference'
+  import { ApiReference } from '@scalar/api-reference'
   import '@scalar/api-reference/style.css'
 
   const { openAPISpecUrl } = useAppConfig()
