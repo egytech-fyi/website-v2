6adbe2ab6728733558c18da2984b401614728801
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-07-23 01:22:44 +0300

    refactor: override default 'LayoutMobileNav' and open sheet from the right

---
+++ app/components/layout/LayoutMobileNav.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  const open = ref(false)
+</script>
+
+<template>
+  <UiSheet v-model:open="open">
+    <UiSheetTrigger as-child>
+      <UiButton variant="ghost" size="icon" class="md:hidden">
+        <Icon name="lucide:menu" size="18" />
+      </UiButton>
+    </UiSheetTrigger>
+
+    <UiSheetContent side="right" class="pr-0">
+      <LayoutHeaderLogo />
+      <LayoutAside is-mobile />
+    </UiSheetContent>
+  </UiSheet>
+</template>
